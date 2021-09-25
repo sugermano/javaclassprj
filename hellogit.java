545b8b05e9d10d9795fba761ab3fa7651557d252
oysuger <91315335+sugermano@users.noreply.github.com>
2021-09-25 13:59:33 +0800

    hello
    hello git

--- hellogit.java
+++ hellogit.java
@@ -1,10 +1,10 @@
 package javaclassprj;
 
-public class javaclassprj {
+public class hellogit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world!");
+		System.out.println("helloworld!");
 	}
 
 }
