9f382c582e6512cac7ef4808c48ffdfd809bd90a
oysuger <2019212212170@stu.hznu.edu.cn>
2021-09-25 13:54:10 +0800

    hello github
    hello github

---
+++ javaclassprj.java
@@ -0,0 +1,10 @@
+package javaclassprj;
+
+public class javaclassprj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+	}
+
+}
